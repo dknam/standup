a20969f2ed313d7a7bae82ecbc6a00e9349758d2
ecount-mac <ecount-mac@ecount-macui-iMac.local>
2018-07-24 21:07:34 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Stand Up 앱을 만들어 봅시다</h1>
+        </header>
+        <p className="App-intro">
+          이제 여기서 부터 프로젝트는 시작입니다. 로고는 일단은 그냥 둡시다.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
